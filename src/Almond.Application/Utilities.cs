dce381a3091e2c78d0acf7754263e2d1a28d933f
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2024-04-09 13:43:29 +0800

    Next (#1)
    * add dockerfile for gateway
    
    * small fix

---
+++ src/Almond.Application/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Almond.Application.Utilities;
+
+public static class Utilities
+{
+    public static void CopyConfigureFile(string source, string dest)
+    {
+        var dir = Path.GetDirectoryName(dest);
+
+        if (dir is not null)
+        {
+            Directory.CreateDirectory(dir);
+            File.Copy(source, dest);
+        }
+    }
+}
