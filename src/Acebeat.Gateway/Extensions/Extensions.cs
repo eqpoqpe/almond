e761baa8c721d2f356e5424d8e6b8653954acef3
Ryan Martin <eqpoqpe@gmail.com>
2024-04-18 08:15:20 +0800

    small fix

--- src/Acebeat.Gateway/Extensions/Extensions.cs
+++ src/Acebeat.Gateway/Extensions/Extensions.cs
@@ -1,4 +1,31 @@
 internal static class Extensions
 {
-    public static void AddApplicationServices(this IHostApplicationBuilder builder) { }
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        const string configureFilePath = "/etc/almond/gateway/appsettings.json";
+
+        if (!builder.Environment.IsDevelopment() && !File.Exists(configureFilePath))
+            Utilities.CopyConfigureFile("./appsettings.json", configureFilePath);
+
+        IConfigurationSection configuration = !builder.Environment.IsDevelopment() ? builder
+           .Configuration
+           .AddJsonFile(configureFilePath)
+           .Build()
+           .GetSection("ReverseProxy") : builder
+           .Configuration
+           .GetSection("ReverseProxy");
+
+        builder.Services.AddReverseProxy().LoadFromConfig(configuration);
+    }
+
+    public static WebApplication BuildAlmondGatewayApplication(
+        this WebApplicationBuilder applicationBuilder)
+    {
+        var app = applicationBuilder.Build();
+
+        app.UseHttpsRedirection();
+        app.MapReverseProxy();
+
+        return app;
+    }
 }
--- src/Acebeat.Gateway/Program.cs
+++ src/Acebeat.Gateway/Program.cs
@@ -1,25 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-const string configureFilePath = "/etc/almond/gateway/appsettings.json";
-
-if (!builder.Environment.IsDevelopment() && !File.Exists(configureFilePath))
-    Utilities.CopyConfigureFile("./appsettings.json", configureFilePath);
-
 builder.AddApplicationServices();
 
-IConfigurationSection configuration = !builder.Environment.IsDevelopment() ? builder
-           .Configuration
-           .AddJsonFile(configureFilePath)
-           .Build()
-           .GetSection("ReverseProxy") : builder
-           .Configuration
-           .GetSection("ReverseProxy");
-
-builder.Services.AddReverseProxy().LoadFromConfig(configuration);
-
-var app = builder.Build();
-
-app.UseHttpsRedirection();
-app.MapReverseProxy();
+var app = builder.BuildAlmondGatewayApplication();
 
 app.Run();
